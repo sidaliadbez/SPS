cbdce26e80f3c66b7b5b3ea055174217cbf7fb73
sidaliadbez <hs_zebda@esi.dz>
2020-06-06 01:51:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sps/UrgenceActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class UrgenceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_urgence)
+    }
+}
