e99bf498399896179ee2cab1c7437a42abdcd274
sidaliadbez <hs_zebda@esi.dz>
2020-06-06 08:43:10 +0100

    card Urgence V1.2

--- app/src/main/java/com/example/sps/UrgenceActivity.kt
+++ app/src/main/java/com/example/sps/UrgenceActivity.kt
@@ -2,6 +2,8 @@ package com.example.sps
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_urgence.*
 
 
 class UrgenceActivity : AppCompatActivity() {
@@ -9,5 +11,22 @@ class UrgenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_urgence)
+        val Urgence =Urgence("Urgence 1 ", "Ceci est une Urgence")
+        val Urgence2 =Urgence("Urgence 2 ", "Ceci est une Urgence")
+         var urgences= ArrayList<Urgence>()
+
+    urgences.add(Urgence)
+        urgences.add(Urgence2)
+        setupRecyclerView(urgences)
+    }
+    private fun setupRecyclerView(urgences: ArrayList<Urgence>) {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerview.layoutManager = layoutManager
+        val adapter = ListeUrgenceAdapter(
+            this,
+             urgences
+        )
+        recyclerview.adapter = adapter
     }
 }
